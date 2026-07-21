9a3b0e280ca249944ef45139345197ed905615d3
Amlan Das <amlan.das.praxis@gmail.com>
2026-07-21 12:14:51 +0530

    portfolio created by angular

---
+++ basicportfolio/src/app/app.ts
@@ -0,0 +1,23 @@
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('amlan');
+  activeSection = signal<'home' | 'about' | 'contact'>('home');
+
+  scrollToSection(sectionId: 'home' | 'about' | 'contact') {
+    this.activeSection.set(sectionId);
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+}
+
